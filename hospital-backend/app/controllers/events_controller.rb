e478d44c3a2953564f5258b3e7083798a3266e5f
nicholasykng <nicholasykng@gmail.com>
2020-03-12 21:13:37 -0700

    adding controller

--- hospital-backend/app/controllers/events_controller.rb
+++ hospital-backend/app/controllers/events_controller.rb
@@ -1,2 +1,23 @@
 class EventsController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+    def events_params
+    end
+
+
+
 end
--- hospital-backend/app/controllers/patients_controller.rb
+++ hospital-backend/app/controllers/patients_controller.rb
@@ -1,2 +1,22 @@
 class PatientsController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+    def patients_params
+    end
+
+    
 end
