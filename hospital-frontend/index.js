2f8c751c5a24c5b593f1727c3c0ab96eb57560ec
nicholasykng <nicholasykng@gmail.com>
2020-03-13 20:30:24 -0700

    added form to create patient

---
+++ hospital-frontend/index.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    //getPatients();
+    Patient.newPatientForm()
+})
---
+++ hospital-frontend/patient.js
@@ -0,0 +1,37 @@
+const patientForm = `
+<label>Name:</label><br>
+<input type="text" id="name><br>
+<input type="hidden" id="dogId"><br>
+<label>Sex At Birth:</label><br>
+<input type="text" id="sex"><br>
+<label>Gender:</label><br>
+<input type="text" id="gender"><br>
+<label>Age:</label><br>
+<input type="integer" id="age"><br>
+<label>Date Of Birth:</label><br>
+<input type="text" id="date_of_birth"><br>
+<input type="submit" value="Add New Patient">
+</form>`
+
+class Patient {
+    constructor(data) {
+        this.id = data.id
+        this.name = data.name
+        this.sex = data.sex
+        this.gender = data.gender
+        this.age = data.age
+        this.date_of_birth = data.date_of_birth
+    }
+    static newPatientForm() {
+        let newPatientForm = document.querySelector('div#record-form')
+        newPatientForm.innerHTML = `<form onsubmit="createPatient(); return false;">` + patientForm
+    }
+}
+
+function getPatients() {
+    fetch("http://localhost:3000/patients")
+    .then(response => response.json())
+    .then(data => {
+        renderPatients(data)
+    })
+}
